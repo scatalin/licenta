cbb80e0c350bea1c8e8ce3eddb30a338ade1d8be
Catalin Stan <stan.catalin.gabriel@gmail.com>
2015-02-22 00:04:21 +0200

    add words from the input text files to the dictionary

--- AutoCompletionSystem/src/main/java/input/InputFilesProcessor.java
+++ AutoCompletionSystem/src/main/java/input/InputFilesProcessor.java
@@ -1,39 +1,65 @@
 package input;
 
-import java.io.File;
+import model.Dictionary;
+
+import java.io.*;
 
 /**
  * Created by Catalin on 2/21/2015 .
  */
 public class InputFilesProcessor {
 
+    private static final String WORD_SEPARATION_REGEX = " ";
     private String inputDirectory;
     private String processedDirectory;
     private File inputDir;
     private File processedDir;
 
+    private Dictionary dictionary;
 
-    public InputFilesProcessor() {
+
+    public InputFilesProcessor(Dictionary dictionary) {
+        this.dictionary = dictionary;
         inputDirectory = Properties.INPUT_FILES_DIRECTORY;
         processedDirectory = Properties.PROCESSED_FILES_DIRECTORY;
         inputDir = new File(inputDirectory);
         if (!inputDir.exists() && !inputDir.isDirectory()) {
-            System.out.println("input files directory does not exist");
+            System.out.println("input files directory does not exist: " + inputDir + ";");
         }
         processedDir = new File(processedDirectory);
         if (!processedDir.exists() && !processedDir.isDirectory()) {
-            System.out.println("processed files directory does not exist");
+            System.out.println("processed files directory does not exist: " + processedDir + ";");
         }
     }
 
     public void processInputFiles() {
-        String[] list = inputDir.list();
-        for (String s : list){
-            System.out.println(s);
+        dictionary.readDictionary();
+        String[] listFileNames = inputDir.list();
+        for (String fileName : listFileNames) {
+            System.out.println(fileName);
+            File file = new File(inputDir+"\\"+fileName);
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line = reader.readLine();
+                while(line != null){
+                    String[] words = line.split(WORD_SEPARATION_REGEX);
+                    for(String word: words){
+                        dictionary.addWord(word);
+                    }
+                    line = reader.readLine();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        dictionary.sortDictionary();
+        dictionary.saveDictionary();
     }
 
 
     // read all input files and hand over to delegates, word processor and phrase processor
 
 }
+
