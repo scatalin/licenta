b0696fdba33c9fdc2fc169b2ebada58969dc661c
Catalin Stan <stan.catalin.gabriel@gmail.com>
2015-03-10 08:58:04 +0200

    factory that builds a tst from a dictionary
    random or in order of the words

---
+++ AutoCompletionSystem/src/main/java/algorithms/tst/extras/TernarySearchTreeFactory.java
@@ -0,0 +1,38 @@
+package algorithms.tst.extras;
+
+import algorithms.tst.TernarySearchTree;
+import algorithms.tst.TernarySearchTreeRecursive;
+import model.dictionary.Dictionary;
+import model.dictionary.Word;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Catalin on 3/9/2015 .
+ */
+public class TernarySearchTreeFactory {
+    public static TernarySearchTree buildTst(Dictionary dictionary) {
+        TernarySearchTree tst = new TernarySearchTreeRecursive();
+        for(Word word :dictionary.getWords()){
+            tst.insert(word.getWord());
+        }
+        return tst;
+    }
+
+    public static TernarySearchTree buildRandomTst(Dictionary dictionary) {
+        TernarySearchTree tst = new TernarySearchTreeRecursive();
+        List<Word> words = new ArrayList<Word>(dictionary.getWords());
+        int middlePosition = words.size()/2;
+        String[] rootWord = new String[1];
+        rootWord[0] = words.remove(middlePosition).getWord();
+        tst.load(rootWord);
+        Random random = new Random();
+        while (words.size()>0){
+            int position = random.nextInt(words.size());
+            tst.insert(words.remove(position).getWord());
+        }
+        return tst;
+    }
+}
