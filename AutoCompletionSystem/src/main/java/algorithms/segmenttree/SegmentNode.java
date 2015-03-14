162263de99b2b833f4c813ee25c2f9e94401d4b0
Catalin Stan <stan.catalin.gabriel@gmail.com>
2015-03-14 15:22:45 +0200

    bugfix segment node

--- AutoCompletionSystem/src/main/java/algorithms/segmenttree/SegmentNode.java
+++ AutoCompletionSystem/src/main/java/algorithms/segmenttree/SegmentNode.java
@@ -18,18 +18,10 @@ public class SegmentNode {
         this.interval = interval;
     }
 
-    public void setRightLimit(int size) {
-        interval.rightLimit = size;
-    }
-
     public int createChildren() {
         depth = 1;
         interval.middle = interval.leftLimit + ((interval.rightLimit - interval.leftLimit) / 2);
-
-        if(interval.middle == interval.leftLimit){
-            interval.middle++;
-        }
-        if(interval.middle == interval.rightLimit){
+        if (interval.leftLimit == interval.rightLimit) {
             return 1;
         }
 
@@ -40,31 +32,51 @@ public class SegmentNode {
         leftInterval.rightLimit = interval.middle;
         leftChild = new SegmentNode(leftInterval);
         int leftDepth = leftChild.createChildren();
-        if(maxDepth < leftDepth){
+        if (maxDepth < leftDepth) {
             maxDepth = leftDepth;
         }
 
         Interval rightInterval = new Interval();
-        rightInterval.leftLimit = interval.middle+1;
+        rightInterval.leftLimit = interval.middle + 1;
         rightInterval.rightLimit = interval.rightLimit;
         rightChild = new SegmentNode(rightInterval);
         int rightDepth = rightChild.createChildren();
-        if(maxDepth < rightDepth){
+        if (maxDepth < rightDepth) {
             maxDepth = rightDepth;
         }
 
-        depth = maxDepth+1;
+        depth = maxDepth + 1;
         return depth;
     }
 
     private class Interval {
-
         int leftLimit = 0;
         int rightLimit = 0;
         int middle = 0;
+
+        @Override
+        public String toString() {
+            return "[" + leftLimit + "," + rightLimit + "]";
+        }
     }
 
-    public int getDepth(){
+    public int getLeftLimit() {
+        return interval.leftLimit;
+    }
+
+    public void setLeftLimit(int leftLimit) {
+        this.interval.leftLimit = leftLimit;
+    }
+
+    public int getRightLimit() {
+        return interval.rightLimit;
+    }
+
+    public void setRightLimit(int rightLimit) {
+        this.interval.rightLimit = rightLimit;
+    }
+
+    public int getDepth() {
         return depth;
     }
 
@@ -84,7 +96,17 @@ public class SegmentNode {
         this.rightChild = rightChild;
     }
 
-    public String toStringInterval() {
-        return '[' + interval.leftLimit +","+ interval.rightLimit + ']';
+    public String toString() {
+        String left = interval.leftLimit+"";
+        if(interval.leftLimit<10){
+            left = left+" ";
+        }
+        String right = interval.rightLimit+"";
+        if(interval.rightLimit<10){
+            right = right+" ";
+        }
+        return "[" + left + "," + right + "]";
     }
+
+
 }
