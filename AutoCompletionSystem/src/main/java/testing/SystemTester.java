cb096f6a63189e0f79a9989bcb3b7171b07ed87d
Catalin Stan <stan.catalin.gabriel@gmail.com>
2015-04-16 00:21:12 +0300

    change system simple testing

--- AutoCompletionSystem/src/main/java/testing/SystemTester.java
+++ AutoCompletionSystem/src/main/java/testing/SystemTester.java
@@ -60,87 +60,7 @@ public class SystemTester {
         manager = new FileManager();
     }
 
-    public void testSystem() throws FileNotFoundException {
-        File[] listFileNames = testDir.listFiles();
-
-        if (listFileNames == null) {
-            System.out.println("an exception occurred while reading files from " + testDir);
-            return;
-        }
-
-        PrintWriter printWriter = new PrintWriter(reportFile);
-
-        //todo read all input files and hand over to delegates, word processor and phrase processor
-        for (File file : listFileNames) {
-            System.out.println("processing file " + file.getName());
-            try {
-                Statistics statistics = new Statistics(file.getName(), 0);
-                statisticsList.add(statistics);
-
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                String line = reader.readLine();
-
-                while (line != null) {
-                    String[] words = line.split(WORD_SEPARATION_REGEX);
-                    for (String word : words) {
-                        word = word.toLowerCase();
-                        statistics.beginWordStatistics(word);
-                        if (word.length() > Properties.AUTOCOMPLETION_THRESHOLD) {
-                            boolean done = false;
-                            int index = 1;
-                            segmentTree.resetSearchK();
-                            while (!done) {
-                                if (index >= word.length()) {
-                                    break;
-                                }
-                                String prefix = word.substring(0, index);
-                                List<Word> completedWords = segmentTree.getNextTopK(prefix);
-                                if (completedWords.isEmpty()) {
-                                    printWriter.println("prefix " + prefix + " from word " + word + " generated no output");
-                                }
-                                int found = -1;
-                                for (int i = 0; i < completedWords.size(); i++) {
-                                    Word aWord = completedWords.get(i);
-                                    if (aWord.getWord().equals(word)) {
-                                        found = i;
-                                        break;
-                                    }
-                                }
-                                if (found != -1) {
-                                    statistics.interrogationStatistic(prefix.length(), found + 1);
-                                }
-                                if (found == 0) {
-                                    done = true;
-                                }
-                                index++;
-                            }
-                        }
-                    }
-                    line = reader.readLine();
-                }
-
-                reader.close();
-                System.out.println("file " + file.getName() + " was tested");
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        printWriter.println("statistics are:");
-
-        for (Statistics statistics : statisticsList) {
-            printWriter.println(statistics);
-        }
-
-
-        printWriter.flush();
-        printWriter.close();
-    }
-
-    public void enhancedTesting(boolean move) throws FileNotFoundException {
+    public void testSystem(boolean move) throws FileNotFoundException {
         File[] listFileNames = testDir.listFiles();
 
         if (listFileNames == null) {
@@ -195,7 +115,7 @@ public class SystemTester {
                     //while line
 
                     statistics.makeAverages();
-                    printWriter.println(statistics.printStatistics());
+                    printWriter.println(statistics.printStatistics(true));
 
                     reader.close();
                     System.out.println("file " + file.getName() + " was tested");
