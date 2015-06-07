09f2ca11a1444520e4013ef8f5f8c6f87db497e8
Catalin Stan <stan.catalin.gabriel@gmail.com>
2015-06-07 22:21:09 +0300

    made ten fold testing suitable for new needs

--- AutoCompletionSystem/src/main/java/testing/SystemTenFoldingTesting.java
+++ AutoCompletionSystem/src/main/java/testing/SystemTenFoldingTesting.java
@@ -1,11 +1,11 @@
 package testing;
 
-import algorithms.SearchTree;
-import algorithms.segmenttree.SegmentTreeLinear;
 import dictionary.Dictionary;
 import dictionary.Word;
 import input.DictionaryProcessor;
 import input.FilesProcessor;
+import system.AutoCompletionSystem;
+import system.AutoCompletionSystemImpl;
 import system.Properties;
 
 import java.io.File;
@@ -19,20 +19,20 @@ import java.util.List;
  */
 public class SystemTenFoldingTesting {
 
-    private final File allReportFile;
+    private File reportFile1;
+    private File reportFile2;
 
-    private SearchTree tree;
+    private AutoCompletionSystem system;
+    private AutoCompletionSystem systemNew;
 
     private FilesProcessor filesProcessor;
 
     private int numberOfFiles;
+    private int dictionarySize;
+    private int noTrainingFiles;
 
     public SystemTenFoldingTesting() {
-
-        allReportFile = new File(Properties.RESULT_DIRECTORY + Properties.SYSTEM_PATH_SEPARATOR + Properties.MEASURES_OUTPUT_FILE_NAME);
-        if (!allReportFile.exists() && !allReportFile.isFile()) {
-            System.out.println("all report file does not exist " + allReportFile + ";");
-        }
+        createReportFiles();
 
         File rotationTestDir = new File(Properties.TEST_ROTATION_DIRECTORY);
         if (!rotationTestDir.exists() && !rotationTestDir.isDirectory()) {
@@ -43,26 +43,50 @@ public class SystemTenFoldingTesting {
         filesProcessor = new FilesProcessor(dictionaryProcessor, Properties.TEST_ROTATION_DIRECTORY);
         numberOfFiles = filesProcessor.getNumberOfFiles();
 
-        tree = new SegmentTreeLinear();
+        system = new AutoCompletionSystemImpl();
+        systemNew = new AutoCompletionSystemImpl();
     }
 
-    public void testSystem() throws FileNotFoundException {
-
-        PrintWriter printWriter = new PrintWriter(allReportFile);
-
-        List<Dictionary> dictionaries = filesProcessor.createDictionariesFromFiles();
-        for (Dictionary dictionary : dictionaries) {
-
-            System.out.println("dictionary for the file" + dictionary.getFileName() + " " + dictionary);
+    private void createReportFiles() {
+        reportFile1 = new File(Properties.RESULT_DIRECTORY + Properties.SYSTEM_PATH_SEPARATOR + Properties.MEASURES_OUTPUT_FILE_NAME);
+        if (!reportFile1.exists() && !reportFile1.isFile()) {
+            System.out.println("all report file does not exist " + reportFile1 + ";");
         }
 
-        //for all sizes of training domain
-        for (int noTrainingFiles = 1; noTrainingFiles < numberOfFiles; noTrainingFiles++) {
+        reportFile2 = new File(Properties.RESULT_DIRECTORY + Properties.SYSTEM_PATH_SEPARATOR + Properties.REPORT_OUTPUT_FILE_NAME);
+        if (!reportFile2.exists() && !reportFile2.isFile()) {
+            System.out.println("all report file does not exist " + reportFile2 + ";");
+        }
+    }
 
-            List<List<Statistics>> trainingStatistics = new ArrayList<>();
-            for (int i = 0; i <= Properties.TEST_WORD_DEPTH; i++) {
-                trainingStatistics.add(new ArrayList<>());
-            }
+    public void testSystem(Dictionary dictionary,boolean loadDefault) throws FileNotFoundException {
+
+
+        List<Dictionary> dictionaries = filesProcessor.createDictionariesFromFiles();
+
+        //for all sizes of training domain
+        for (noTrainingFiles = 0; noTrainingFiles < numberOfFiles; noTrainingFiles++) {
+
+            int position = Properties.MEASURES_OUTPUT_FILE_NAME.lastIndexOf('-');
+            Properties.MEASURES_OUTPUT_FILE_NAME = Properties.MEASURES_OUTPUT_FILE_NAME.substring(0, position + 1) + noTrainingFiles;
+
+            position = Properties.REPORT_OUTPUT_FILE_NAME.lastIndexOf('-');
+            Properties.REPORT_OUTPUT_FILE_NAME = Properties.REPORT_OUTPUT_FILE_NAME.substring(0, position + 1) + noTrainingFiles;
+
+            createReportFiles();
+
+            PrintWriter printWriter1 = new PrintWriter(reportFile1);
+            PrintWriter printWriter2 = new PrintWriter(reportFile2);
+
+            List<Statistics> stat0 = new ArrayList<>();
+            List<Statistics> stat1 = new ArrayList<>();
+            List<Statistics> stat2 = new ArrayList<>();
+            List<Statistics> stat3 = new ArrayList<>();
+
+            List<Statistics> statNew0 = new ArrayList<>();
+            List<Statistics> statNew1 = new ArrayList<>();
+            List<Statistics> statNew2 = new ArrayList<>();
+            List<Statistics> statNew3 = new ArrayList<>();
 
             //todo read all input files and hand over to delegates, word processor and phrase processor
 
@@ -70,66 +94,150 @@ public class SystemTenFoldingTesting {
             for (int testFileIndex = 0; testFileIndex < numberOfFiles; testFileIndex++) {
 
                 //build train dictionary
-                Dictionary trainingDictionary = new Dictionary();
-
-                //iterate all files
-                int count = 0;
-                int index = -1;
-                while (count != noTrainingFiles) {
-                    index++;
-                    //skip the test file
-                    if (index == testFileIndex) {
-                        continue;
-                    }
-                    //concatenate the rest of the dictionaries
-                    Dictionary addDictionary = dictionaries.get(index);
-                    for (Word word : addDictionary.asList()) {
+                Dictionary trainingDictionary = buildTrainingDictionary(dictionaries, noTrainingFiles, testFileIndex);
+                if(loadDefault){
+                    for (Word word : dictionary.asList()) {
                         trainingDictionary.addDefaultWord(word);
                     }
-
-                    count++;
                 }
 
                 //construct the tree with the training Dictionary
+                system.loadCustomDictionary(trainingDictionary);
+                system.constructSearchTree();
 
-                tree.reset();
-                tree.setNumberOfSuggestions(Properties.AUTOCOMPLETION_SUGGESTION_SIZE);
-                tree.load(trainingDictionary.getWordsSortedByWeight(), true);
-                int dictionarySize = trainingDictionary.getNumberOfWords();
+                //construct the tree with the training Dictionary
+                systemNew.loadCustomDictionary(trainingDictionary);
+                systemNew.constructSearchTree();
+
+                dictionarySize = trainingDictionary.getNumberOfWords();
 
                 Dictionary testDictionary = dictionaries.get(testFileIndex);
                 System.out.println("testing " + testDictionary.getFileName() + " for training size " + noTrainingFiles);
 
+                ////////////////////////////////////////////////// 1
+                Statistics statistics = testSystemWithDictionary(testDictionary, false, system);
 
-                for (int currentRun = Properties.TEST_WORD_START; currentRun <= Properties.TEST_WORD_DEPTH; currentRun++) {
+                stat0.add(statistics);
+                printWriter1.println(statistics.printStatistics(true));
+                printWriter1.flush();
 
-                    Statistics statistics = new Statistics(testDictionary.getFileName(), currentRun, dictionarySize, noTrainingFiles);
+                statistics = testSystemWithDictionary(testDictionary, false, systemNew);
 
-                    for (Word w : testDictionary.asList()) {
-                        String word = w.getWord().toLowerCase();
-                        if (word.length() >= Properties.AUTOCOMPLETION_THRESHOLD) {
-                            statistics.beginWordStatistics(word);
-                            tree.resetCompletion();
-                            String prefix = word.substring(0, Math.min(currentRun, word.length()));
+                statNew0.add(statistics);
+                printWriter2.println(statistics.printStatistics(true));
+                printWriter2.flush();
 
-                            List<String> completedWords = tree.getSuggestions(prefix);
-                            fillStatistics(statistics, word, prefix.length(), completedWords);
-                        }
-                    }
+                ////////////////////////////////////////////////// 1
+                statistics = testSystemWithDictionary(testDictionary, true, system);
 
-                    statistics.makeAverages();
-                    trainingStatistics.get(currentRun).add(statistics);
+                stat1.add(statistics);
+                printWriter1.println(statistics.printStatistics(true));
+                printWriter1.flush();
 
+                statistics = testSystemWithDictionary(testDictionary, true, systemNew);
+
+                statNew1.add(statistics);
+                printWriter2.println(statistics.printStatistics(true));
+                printWriter2.flush();
+
+                ////////////////////////////////////////////////// 2
+                statistics = testSystemWithDictionary(testDictionary, true, system);
+
+                stat2.add(statistics);
+                printWriter1.println(statistics.printStatistics(true));
+                printWriter1.flush();
+
+                statistics = testSystemWithDictionary(testDictionary, false, systemNew);
+
+                statNew2.add(statistics);
+                printWriter2.println(statistics.printStatistics(true));
+                printWriter2.flush();
+
+                ////////////////////////////////////////////////// 3
+                statistics = testSystemWithDictionary(testDictionary, false, system);
+
+                stat3.add(statistics);
+                printWriter1.println(statistics.printStatistics(true));
+                printWriter1.flush();
+
+                statistics = testSystemWithDictionary(testDictionary, false, systemNew);
+
+                statNew3.add(statistics);
+                printWriter2.println(statistics.printStatistics(true));
+                printWriter2.flush();
+
+            }
+
+            printAverageForList(printWriter1, stat0);
+
+            printAverageForList(printWriter1, stat1);
+
+            printAverageForList(printWriter1, stat2);
+
+            printAverageForList(printWriter1, stat3);
+
+            printAverageForList(printWriter2, statNew0);
+
+            printAverageForList(printWriter2, statNew1);
+
+            printAverageForList(printWriter2, statNew2);
+
+            printAverageForList(printWriter2, statNew3);
+
+            printWriter1.close();
+            printWriter2.close();
+        }
+    }
+
+    private Dictionary buildTrainingDictionary(List<Dictionary> dictionaries, int noTrainingFiles, int testFile) {
+        Dictionary trainingDictionary = new Dictionary();
+
+        //iterate all files
+        int count = 0;
+        int index = -1;
+        while (count != noTrainingFiles) {
+            index++;
+            //skip the test file
+            if (index == testFile) {
+                continue;
+            }
+            //concatenate the rest of the dictionaries
+            Dictionary addDictionary = dictionaries.get(index);
+            for (Word word : addDictionary.asList()) {
+                trainingDictionary.addDefaultWord(word);
+            }
+
+            count++;
+        }
+        return trainingDictionary;
+    }
+
+    private Statistics testSystemWithDictionary(Dictionary testDictionary, boolean learn, AutoCompletionSystem system) {
+        Statistics statistics = new Statistics(testDictionary.getFileName(), Properties.AUTOCOMPLETION_THRESHOLD, dictionarySize, noTrainingFiles);
+        for (Word w : testDictionary.asList()) {
+            String word = w.getWord().toLowerCase();
+            if (word.length() >= Properties.AUTOCOMPLETION_THRESHOLD) {
+                statistics.beginWordStatistics(word);
+                system.startCompletion();
+                String prefix = word.substring(0, Properties.AUTOCOMPLETION_THRESHOLD);
+
+                List<String> completedWords = system.getCompletion(prefix);
+                fillStatistics(statistics, word, prefix.length(), completedWords);
+
+                if (learn) {
+                    system.selectWord(word);
                 }
             }
-            for (int currentRun = Properties.TEST_WORD_START; currentRun <= Properties.TEST_WORD_DEPTH; currentRun++) {
-                System.out.println("making average for training size " + noTrainingFiles + " and threshold " + currentRun);
-                Statistics averageStatistic = makeAverages(trainingStatistics.get(currentRun), currentRun, noTrainingFiles);
-                printWriter.println(averageStatistic.printStatistics(false));
-                printWriter.flush();
-
-            }
         }
+        statistics.makeAverages();
+        return statistics;
+    }
+
+    private void printAverageForList(PrintWriter printWriter, List<Statistics> stat) {
+        System.out.println("making average for training size " + noTrainingFiles);
+        Statistics averageStatistic = makeAverages(stat, 0, noTrainingFiles);
+        printWriter.println(averageStatistic.printStatistics(false));
+        printWriter.flush();
     }
 
     private void fillStatistics(Statistics statistics, String word, int prefixLength, List<String> completedWords) {
@@ -156,6 +264,7 @@ public class SystemTenFoldingTesting {
 
     private Statistics makeAverages(List<Statistics> statistics, int currentRun, int noTrainingFiles) {
         Statistics averageStatistic = new Statistics("", currentRun, noTrainingFiles);
+        int size = statistics.size();
         long totalTotal = 0;
         long totalSuccessful = 0;
         long totalOutRange = 0;
@@ -164,24 +273,24 @@ public class SystemTenFoldingTesting {
         long totalDictionarySize = 0;
         double totalPrecision = 0.0;
         double totalRecall = 0.0;
-        for (Statistics statistic : statistics) {
-            totalDictionarySize += statistic.getDictionarySize();
-            totalTotal += statistic.getTotal();
-            totalSuccessful += statistic.getSuccessful();
-            totalNotFound += statistic.getWordNotInTree();
-            totalNotInTree += statistic.getNoSuggestionsForPrefix();
-            totalOutRange += statistic.getOutRange();
-            totalPrecision += statistic.getPrecision();
-            totalRecall += statistic.getRecall();
+        for (Statistics stat : statistics) {
+            totalDictionarySize += stat.getDictionarySize();
+            totalTotal += stat.getTotal();
+            totalSuccessful += stat.getSuccessful();
+            totalNotFound += stat.getWordNotInTree();
+            totalNotInTree += stat.getNoSuggestionsForPrefix();
+            totalOutRange += stat.getOutRange();
+            totalPrecision += stat.getPrecision();
+            totalRecall += stat.getRecall();
         }
-        averageStatistic.setTotal(totalTotal / statistics.size());
-        averageStatistic.setDictionarySize(totalDictionarySize / statistics.size());
-        averageStatistic.setWordNotInTree(totalNotFound / statistics.size());
-        averageStatistic.setNoSuggestionsForPrefix(totalNotInTree / statistics.size());
-        averageStatistic.setOutRange(totalOutRange / statistics.size());
-        averageStatistic.setPrecision(totalPrecision / statistics.size());
-        averageStatistic.setSuccessful(totalSuccessful / statistics.size());
-        averageStatistic.setRecall(totalRecall / statistics.size());
+        averageStatistic.setTotal(totalTotal / size);
+        averageStatistic.setDictionarySize(totalDictionarySize / size);
+        averageStatistic.setWordNotInTree(totalNotFound / size);
+        averageStatistic.setNoSuggestionsForPrefix(totalNotInTree / size);
+        averageStatistic.setOutRange(totalOutRange / size);
+        averageStatistic.setPrecision(totalPrecision / size);
+        averageStatistic.setSuccessful(totalSuccessful / size);
+        averageStatistic.setRecall(totalRecall / size);
 
         return averageStatistic;
     }
